049dc9d582c2694abf66d81fceb518ca314f0bc5
NickHamann <79927008+NickHamann@users.noreply.github.com>
2021-04-27 14:39:35 -0400

    Update roboDescription.m

--- roboDescription.m
+++ roboDescription.m
@@ -1,6 +1,8 @@
 function roboDescription()
 %roboDescription() prints a description of what the robotic arm does and
 %describes in more detail what each option of the menu is.
+%
+% Format: roboDescri.
 cprintf('#007299','Assign countries & box type to containers\n');
 help shippingDetails
 
@@ -13,7 +15,16 @@ help loadContainers
 cprintf('#007299','Container stats\n');
 help containerStats
 
-cprintf('#007299','Move to home position\n');
-help homePosition
+cprintf('#007299','Display Countries\n');
+help dispCountries
+
+cprintf('#007299','Move Arm\n');
+help moveArm
+
+cprintf('#007299','Start Up Routine\n');
+help startUpRoutine
+
+cprintf('#007299','Move Servo\n');
+help moveservo
 end
 
