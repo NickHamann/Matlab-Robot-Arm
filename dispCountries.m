a87293351fac9f2c0e8ec5339a61d6e0303ce643
NickHamann <79927008+NickHamann@users.noreply.github.com>
2021-04-27 14:52:26 -0400

    small change

--- dispCountries.m
+++ dispCountries.m
@@ -2,7 +2,7 @@ function dispCountries(Containers, isCoordinatesSet)
 % dispCountries displays the Country and box type of each Container. It
 % also receives a boolean isCoordinatesSet which will additionally display 
 % the X and Y coordinates of each container.
-
+%
 % Format: dispCountries(Containers, isCoordinatesSet)
 for i = 1:3
     cprintf([1, 0.5, 0], 'Container %d:\n', i);
